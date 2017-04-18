7de98a7a7d149abb20c1f89a21321b64daf62797
Ansel Blume <anseleblume@gmail.com>
2017-04-18 12:57:45 -0700

    Added tester file.

---
+++ testCombinationGenerator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "CombinationGenerator.h"
+
+void printVector( vector<int> vect );
+
+int main()
+{
+    vector<int> set = { 3, 7, 1, 18, 19, 20 };
+
+    CombinationGenerator * cg = new CombinationGenerator( set, 3 );
+
+    for ( int i = 0; i < 21; i++ )
+    {
+        cout << "Next combination: ";
+        printVector( cg->getNextCombination() );
+        cout << endl;
+    }
+
+
+    cout << "Testing combination choose zero." << endl << endl;
+    delete cg;
+
+    cg = new CombinationGenerator( set, 0 );
+
+    for ( int i = 0; i < 2; i++ )
+    {
+        cout << "Next Combination: ";
+        printVector( cg->getNextCombination() );
+        cout << endl;
+    }
+
+    cout << "Testing combination choose one." << endl << endl;
+    delete cg;
+
+    cg = new CombinationGenerator( set, 1 );
+
+    for ( int i = 0; i < 7; i++ )
+    {
+        cout << "Next combination: ";
+        printVector( cg->getNextCombination() );
+        cout << endl;
+    }
+  
+
+    delete cg;
+    return 0;
+}
+
+void printVector( vector<int> vect )
+{
+    // Prevent empty vector from breaking code
+    if ( !vect.size() )
+    {
+        cout << "<>" << endl;
+        return;
+    }
+
+    // Print vector in format <...,...,...>
+    cout << "<";
+
+    for ( int i = 0; i < vect.size() - 1; i++ )
+        cout << vect[i] << ", ";
+
+    cout << vect[vect.size() - 1] << ">" << endl;
+}
